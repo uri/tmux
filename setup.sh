28dea406a46794c72a4b6d91c05f87d4133d40cd
Uri Gorelik <uri.gore@gmail.com>
2017-09-15 11:26:49 -0400

    Added new terminfo and tmux related progs

---
+++ setup.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+ln -fs $HOME/dev/dotfiles/tmux/tmux.conf $HOME/.tmux.conf
+ln -fs $HOME/dev/dotfiles/tmux/tmux-airline.theme $HOME/.tmux-airline.theme
+
+ln -fs $HOME/dev/dotfiles/tmux/tms $HOME/bin/
+ln -fs $HOME/dev/dotfiles/tmux/tma $HOME/bin/
---
+++ terminfo/setup.sh
@@ -0,0 +1,3 @@
+tic  tmux.terminfo
+tic  tmux-256color.terminfo
+tic  xterm-256color.terminfo
